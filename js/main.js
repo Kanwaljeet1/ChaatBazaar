db93802b8f9cab2b6b079e0880fde792e29b54b1
twinkle0tech <muskantamang925@gmail.com>
2026-05-29 22:57:32 +0530

    adding login/signup button

--- js/main.js
+++ js/main.js
@@ -1434,27 +1434,3 @@ function showSkeletonCartItems(count = 2) {
     cartItemsContainer.appendChild(createSkeletonCartItem());
   }
 }
-// dark-mode
-const toggleBtn = document.getElementById("theme-toggle");
-
-// Load saved theme on page load
-document.addEventListener("DOMContentLoaded", () => {
-  const savedTheme = localStorage.getItem("theme");
-
-  if (savedTheme === "dark") {
-    document.body.classList.add("dark");
-  }
-});
-
-// Toggle dark/light mode
-if (toggleBtn) {
-  toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-  });
-}
